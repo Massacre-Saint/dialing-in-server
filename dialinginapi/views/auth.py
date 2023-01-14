c72ad4b01deb5f03c58881fe0adcaa6d0d2c99f0
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2023-01-14 13:00:55 -0600

    Pushed up bug

---
+++ dialinginapi/views/auth.py
@@ -0,0 +1,51 @@
+"""Module handles user"""
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from dialinginapi.models import User
+
+@api_view(['POST'])
+def check_user(request):
+    """Check to see if user has associated User Class
+
+    Args:
+        request: The full HTTP request object
+    """
+
+    uid = request.data['uid']
+
+    try:
+        user = User.objects.get(uid=uid)
+        data = {
+          'id': user.id,
+          'uid': user.uid,
+          'image_url': user.image_url,
+          'name': user.name
+        }
+        return Response(data)
+
+    except: # pylint: disable=bare-except
+      # Bad login details were provided. So we can't log the user in.
+        data = { 'valid': False }
+        return Response(data)
+
+@api_view(['POST'])
+def register_user(request):
+    '''Handles the creation of a new user for authentication
+
+    Method arguments:
+      request -- The full HTTP request object
+    '''
+
+    user = User.objects.create(
+      uid = request.data['uid'],
+      image_url = request.data['image_url'],
+      name = request.data['name']
+    )
+
+    data = {
+      'id': user.id,
+      'uid': user.method_id,
+      'image_url': user.image_url,
+      'name': user.name
+    }
+    return Response(data)
