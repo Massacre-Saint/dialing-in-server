5192aee4715783af809621859c2542c55a057698
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2023-01-20 20:38:25 -0600

    Pushing up bug

--- dialinginapi/views/owner.py
+++ dialinginapi/views/owner.py
@@ -2,7 +2,7 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers,status
-from dialinginapi.models import Owner, User, Recipe
+from dialinginapi.models import Owner, User, Recipe, Method
 
 class OwnerView(ViewSet):
     """Class creates viewset for Favorite"""
@@ -17,31 +17,13 @@ class OwnerView(ViewSet):
         except Owner.DoesNotExist as ex:
 
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
-    # def list(self, request):
-    #     """Handles GET request for all favorites"""
-    #     owner_recipes = Owner.objects.all()
-    #     if 'Authorization' in request.headers:
-    #         try:
-    #             uid = request.META['HTTP_AUTHORIZATION']
-    #             user = User.objects.get(uid=uid)
-    #             try:
-    #                 user_recipes = owner_recipes.filter(user_id = user.id)
-    #                 serializer = OwnerSerializer(user_recipes, many=True)
-    #                 return Response(serializer.data)
-    #             except Owner.DoesNotExist as ex:
-
-    #                 return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
-
-    #         except User.DoesNotExist as ex:
-
-    #             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
-    #     serializer = OwnerSerializer(owner_recipes, many=True)
-    #     return Response(serializer.data)
+  
     def list(self, request):
         """Handles GET requests for Owner Class"""
         # Decide if auth header is present
         owner_recipes = Owner.objects.all()
         user_id = request.query_params.get('userId', None)
+        method_id = request.query_params.get('methodId', None)
 
         try:
             if user_id is not None:
@@ -65,20 +47,35 @@ class OwnerView(ViewSet):
             # Error handling for user not existing
                 return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
         try:
-            owner_recipes = Owner.objects.all()
-            serializer = OwnerSerializer(owner_recipes, many=True)
+            if method_id is not None:
+                recipes = Recipe.objects.all()
+                user_recipes_by_method = recipes.filter(method_id = method_id, default = False, published = True)
+                final = owner_recipes.filter(recipe_id__id=user_recipes_by_method.method_id)
+                serializer = OwnerSerializer(final, many=True)
+
+                return Response(serializer.data)
 
         except Owner.DoesNotExist as ex:
             # Error handling for no owner recipes
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
 
+        serializer= OwnerSerializer(owner_recipes, many=True)
         return Response(serializer.data)
 class OwnerSerializer(serializers.ModelSerializer):
     """Serilizer for Owner Class"""
     class Meta:
-        depth = 2
+        depth = 1
         model = Owner
         fields = (
+        #     'id',
+        #   'brew_time',
+        #   'grind_id',
+        #   'weight',
+        #   'dose',
+        #   'method_id',
+        #   'recipe_name',
+        #   'default',
+        #   'published',
           'id',
           'user_id',
           'recipe_id',
