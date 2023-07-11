64860fe9c207f9bb41e6c0dad17e796388a0a996
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2023-07-10 19:43:42 -0500

    Added rest_framework to install-apps in settings.py

--- dialingin/settings.py
+++ dialingin/settings.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+import os
+import django_on_heroku
+
 """
 Django settings for dialingin project.
 
@@ -9,9 +13,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os
-import django_on_heroku
-from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('MY_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'dialinginapi'
 ]
 
